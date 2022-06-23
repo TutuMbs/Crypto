8052e771e7ea17c6790b06315ede604c8f811a4a
TutuMbs <84512809+TutuMbs@users.noreply.github.com>
2022-06-22 23:32:11 -0300

    Update cripto2.js

--- cripto2.js
+++ cripto2.js
@@ -1,14 +1,17 @@
 const crypto = require('crypto')
+//algoritmo usado para gerar a informação
 const alg = 'aes-256-ctr'
+//senha para criptografar a string
 const pwd = 'Voyage'
 
 
 const text = 'Mensagem a ser Criptografada'
+//pega o algoritmo e a senha e criptografa
 const cipher = crypto.createCipher(alg, pwd)
 const crypted = cipher.update(text, 'utf8', 'hex')
 console.log('Criptografado: '+crypted)
 
-
+//Descriptografando (fazendo o contrário)
 const decipher = crypto.createDecipher(alg, pwd)
 const plain = decipher.update(crypted, 'hex', 'utf8')
 console.log('Descriptografado: '+plain)
